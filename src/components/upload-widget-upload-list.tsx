50a24e48b371fe6d5903b43740c2504e8c96434f
Rodrigo Cândido <rodrigocan2024@gmail.com>
2025-07-08 16:07:42 -0300

    Componente: Lista de uploads

--- src/components/upload-widget-upload-list.tsx
+++ src/components/upload-widget-upload-list.tsx
@@ -1,3 +1,16 @@
+import { UploadWidgetUploadItem } from './upload-widget-upload-item'
+
 export function UploadWidgetUploadList() {
-  return <div className="px-3 h-48"></div>
+  return (
+    <div className="px-3 flex-col gap-3">
+      <span className="text-xs font-medium">
+        Uploaded files <span className="text-zinc-400">(2)</span>
+      </span>
+
+      <div className="flex flex-col gap-2">
+        <UploadWidgetUploadItem />
+        <UploadWidgetUploadItem />
+      </div>
+    </div>
+  )
 }
