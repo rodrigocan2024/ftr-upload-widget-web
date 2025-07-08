2de98694887b53c89888f683afc95f56ba5128ea
Rodrigo Cândido <rodrigocan2024@gmail.com>
2025-07-08 18:42:27 -0300

    Animações do Widget

--- src/components/upload-widget-dropzone.tsx
+++ src/components/upload-widget-dropzone.tsx
@@ -1,3 +1,4 @@
+import { motion } from 'motion/react'
 import { useDropzone } from 'react-dropzone'
 import CircularProgressBar from './ui/circular-progress-bar'
 
@@ -17,10 +18,15 @@ export function UploadWidgetDropzone() {
   })
 
   return (
-    <div className="px-3 flex flex-col gap-3">
+    <motion.div
+      className="px-3 flex flex-col gap-3"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.2 }}
+    >
       <div
         data-active={isDragActive}
-        className="cursor-pointer text-zinc-400 bg-black/20 p-5 rounded-lg border border-zinc-700 border-dashed h-32 flex flex-col items-center justify-center gap-1 hover:border-zinc-600 transition-colors data-[active=true]:bg-indigo-500/10 data-[active=true]:border-indigo-500 data-[active=true]:text-indigo-400"
+        className="cursor-pointer text-zinc-400 bg-black/20 p-5 rounded-lg border border-zinc-700 border-dashed h-32 flex flex-col items-center justify-center gap-1 hover:border-zinc-600 transition-colors data-[active=true]:bg-indigo-500/10 data-[active=true]:border-indigo-500"
         {...getRootProps()}
       >
         <input type="file" {...getInputProps()} />
@@ -45,6 +51,6 @@ export function UploadWidgetDropzone() {
       <span className="text-xxs text-zinc-400">
         Only PNG and JPG files are supported.
       </span>
-    </div>
+    </motion.div>
   )
 }
