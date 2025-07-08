dbf883c5886b2952a421f4cead10c3a146d789d6
Rodrigo Cândido <rodrigocan2024@gmail.com>
2025-07-07 22:45:01 -0300

    Estrutura do Widget

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,9 @@
+import { UploadWidget } from "./components/upload-widget";
+
 export function App() {
-	return <h1>Hello World</h1>;
+	return (
+		<main className="h-dvh flex flex-col items-center justify-center p-10">
+			<UploadWidget />
+		</main>
+	);
 }
