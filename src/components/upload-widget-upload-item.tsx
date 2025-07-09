ffe64afe7c1579a6e34b5ef90b1f6c50e7a10d6f
Rodrigo Cândido <rodrigocan2024@gmail.com>
2025-07-09 19:56:45 -0300

    Ajustes finais

--- src/components/upload-widget-upload-item.tsx
+++ src/components/upload-widget-upload-item.tsx
@@ -2,19 +2,21 @@ import * as Progress from '@radix-ui/react-progress'
 import { Download, ImageUp, Link2, RefreshCcw, X } from 'lucide-react'
 import { motion } from 'motion/react'
 import { type Upload, useUploads } from '../store/uploads'
+import { downloadUrl } from '../utils/download-url'
 import { formatBytes } from '../utils/format-bytes'
 import { Button } from './ui/button'
 
 interface UploadWidgetUploadItemProps {
-  uploadId: string
   upload: Upload
+  uploadId: string
 }
 
 export function UploadWidgetUploadItem({
-  uploadId,
   upload,
+  uploadId,
 }: UploadWidgetUploadItemProps) {
   const cancelUpload = useUploads((store) => store.cancelUpload)
+  const retryUpload = useUploads((store) => store.retryUpload)
 
   const progress = Math.min(
     upload.compressedSizeInBytes
@@ -35,7 +37,7 @@ export function UploadWidgetUploadItem({
       <div className="flex flex-col gap-1">
         <span className="text-xs font-medium flex items-center gap-1">
           <ImageUp className="size-3 text-zinc-300" strokeWidth={1.5} />
-          <span>{upload.name}</span>
+          <span className="max-w-[180px] truncate">{upload.name}</span>
         </span>
 
         <span className="text-xxs text-zinc-400 flex gap-1.5 items-center">
@@ -83,16 +85,18 @@ export function UploadWidgetUploadItem({
         />
       </Progress.Root>
 
-      <div className="absolute top-2.5 right-2.5 flex items-center gap-1">
+      <div className="absolute top-2 right-2 flex items-center gap-1">
         <Button
           size="icon-sm"
-          aria-disabled={upload.status !== 'success'}
-          asChild
+          aria-disabled={!upload.remoteUrl}
+          onClick={() => {
+            if (upload.remoteUrl) {
+              downloadUrl(upload.remoteUrl)
+            }
+          }}
         >
-          <a href={upload.remoteUrl} download>
-            <Download className="size-4" strokeWidth={1.5} />
-            <span className="sr-only">Download compressed image</span>
-          </a>
+          <Download className="size-4" strokeWidth={1.5} />
+          <span className="sr-only">Download compressed image</span>
         </Button>
 
         <Button
@@ -107,8 +111,9 @@ export function UploadWidgetUploadItem({
         </Button>
 
         <Button
-          size="icon-sm"
           disabled={!['cancelled', 'error'].includes(upload.status)}
+          onClick={() => retryUpload(uploadId)}
+          size="icon-sm"
         >
           <RefreshCcw className="size-4" strokeWidth={1.5} />
           <span className="sr-only">Retry upload</span>
--- src/store/uploads.ts
+++ src/store/uploads.ts
@@ -9,7 +9,7 @@ import { compressImage } from '../utils/compress-image'
 export type Upload = {
   name: string
   file: File
-  abortController: AbortController
+  abortController?: AbortController
   status: 'progress' | 'success' | 'error' | 'cancelled'
   originalSizeInBytes: number
   compressedSizeInBytes?: number
@@ -21,6 +21,7 @@ type UploadState = {
   uploads: Map<string, Upload>
   addUploads: (files: File[]) => void
   cancelUpload: (uploadId: string) => void
+  retryUpload: (uploadId: string) => void
 }
 
 enableMapSet()
@@ -46,6 +47,16 @@ export const useUploads = create<UploadState, [['zustand/immer', never]]>(
         return
       }
 
+      const abortController = new AbortController()
+
+      updateUpload(uploadId, {
+        uploadSizeInBytes: 0,
+        compressedSizeInBytes: undefined,
+        remoteUrl: undefined,
+        abortController,
+        status: 'progress',
+      })
+
       try {
         const compressedFile = await compressImage({
           file: upload.file,
@@ -65,7 +76,7 @@ export const useUploads = create<UploadState, [['zustand/immer', never]]>(
               updateUpload(uploadId, { uploadSizeInBytes: sizeInBytes })
             },
           },
-          { signal: upload.abortController.signal }
+          { signal: abortController.signal }
         )
 
         updateUpload(uploadId, { status: 'success', remoteUrl: url })
@@ -87,18 +98,20 @@ export const useUploads = create<UploadState, [['zustand/immer', never]]>(
         return
       }
 
-      upload.abortController.abort()
+      upload.abortController?.abort()
+    }
+
+    function retryUpload(uploadId: string) {
+      processUpload(uploadId)
     }
 
     function addUploads(files: File[]) {
       for (const file of files) {
         const uploadId = crypto.randomUUID()
-        const abortController = new AbortController()
 
         const upload: Upload = {
           name: file.name,
           file,
-          abortController,
           status: 'progress',
           originalSizeInBytes: file.size,
           uploadSizeInBytes: 0,
@@ -116,6 +129,7 @@ export const useUploads = create<UploadState, [['zustand/immer', never]]>(
       uploads: new Map(),
       addUploads,
       cancelUpload,
+      retryUpload,
     }
   })
 )
