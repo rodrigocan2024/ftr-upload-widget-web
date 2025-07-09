f16bfb713d7f8d6fdedc801456565841014ffeab
Rodrigo Cândido <rodrigocan2024@gmail.com>
2025-07-09 15:57:12 -0300

    Compressão de imagens com canvas

--- src/store/uploads.ts
+++ src/store/uploads.ts
@@ -4,6 +4,7 @@ import { create } from 'zustand'
 import { immer } from 'zustand/middleware/immer'
 import { useShallow } from 'zustand/shallow'
 import { uploadFileToStorage } from '../http/upload-file-to-storage'
+import { compressImage } from '../utils/compress-image'
 
 export type Upload = {
   name: string
@@ -44,9 +45,16 @@ export const useUploads = create<UploadState, [['zustand/immer', never]]>(
       }
 
       try {
+        const compressedFile = await compressImage({
+          file: upload.file,
+          maxWidth: 200,
+          maxHeight: 200,
+          quality: 0.5,
+        })
+
         await uploadFileToStorage(
           {
-            file: upload.file,
+            file: compressedFile,
             onProgress(sizeInBytes) {
               updateUpload(uploadId, { uploadSizeInBytes: sizeInBytes })
             },
