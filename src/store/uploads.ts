2df9b8d952f42a870e6db74061be40bc60983d75
Rodrigo Cândido <rodrigocan2024@gmail.com>
2025-07-09 16:58:46 -0300

    Ajustando progresso dos uploads

--- src/store/uploads.ts
+++ src/store/uploads.ts
@@ -12,7 +12,9 @@ export type Upload = {
   abortController: AbortController
   status: 'progress' | 'success' | 'error' | 'cancelled'
   originalSizeInBytes: number
+  compressedSizeInBytes?: number
   uploadSizeInBytes: number
+  remoteUrl?: string
 }
 
 type UploadState = {
@@ -47,12 +49,16 @@ export const useUploads = create<UploadState, [['zustand/immer', never]]>(
       try {
         const compressedFile = await compressImage({
           file: upload.file,
-          maxWidth: 200,
-          maxHeight: 200,
-          quality: 0.5,
+          maxWidth: 1000,
+          maxHeight: 1000,
+          quality: 0.8,
         })
 
-        await uploadFileToStorage(
+        updateUpload(uploadId, {
+          compressedSizeInBytes: compressedFile.size,
+        })
+
+        const { url } = await uploadFileToStorage(
           {
             file: compressedFile,
             onProgress(sizeInBytes) {
@@ -62,7 +68,7 @@ export const useUploads = create<UploadState, [['zustand/immer', never]]>(
           { signal: upload.abortController.signal }
         )
 
-        updateUpload(uploadId, { status: 'success' })
+        updateUpload(uploadId, { status: 'success', remoteUrl: url })
       } catch (err) {
         if (err instanceof CanceledError) {
           updateUpload(uploadId, { status: 'cancelled' })
@@ -127,8 +133,12 @@ export const usePendingUploads = () => {
 
       const { total, uploaded } = Array.from(store.uploads.values()).reduce(
         (acc, upload) => {
-          acc.total += upload.originalSizeInBytes
-          acc.uploaded += upload.uploadSizeInBytes
+          if (upload.compressedSizeInBytes) {
+            acc.uploaded += upload.uploadSizeInBytes
+          }
+
+          acc.total +=
+            upload.compressedSizeInBytes || upload.originalSizeInBytes
 
           return acc
         },
